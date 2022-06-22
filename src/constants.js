c4116410d9211e096dedd295bd13bda156d955bd
Induwara-Nagodavithana <induwara.nagodavithana@gmail.com>
2022-06-22 16:29:58 +0530

    changed

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
-const constants = { baseUrl: "http://localhost:3000/" };
+const constants = { baseUrl: "https://62b2f5241a4ffc0009f387f6--cute-llama-ad8956.netlify.app/" };
 export default constants;
